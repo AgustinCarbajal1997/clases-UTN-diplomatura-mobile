c9f336e40bfd63e902b14ce7a82c32b12022dfc3
Agustin Carbajal <magustincarbajal97@gmail.com>
2024-09-16 20:31:33 -0300

    dart

---
+++ clase-1/main.dart
@@ -0,0 +1,22 @@
+void main() {
+    // variables 
+    int miVarEntero; // declarar implicitamente
+    miVarEntero = 2; // inicializar
+  
+    int miVarEntero2 = 3;
+  
+    String mi_cadena = 'texto';
+    
+    bool miBooleano = false;
+  
+    double miDecimal = 3.2;
+    
+    String format = "Hola mi nombre es: ";
+    String nombre = "Agustin";
+  
+    int edad = 20;
+    int edad2 = 7;
+  
+    print('$format $nombre, tengo ${edad + edad2}');
+  
+}
